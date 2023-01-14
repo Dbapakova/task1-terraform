31c9c39eabb23435546206ec84a9b1ecf6032593
Islam Kambarov <ikambarov@evolvecyber.com>
2023-01-14 20:33:29 +0000

    Use multiple files

--- Class-1/Example-1/keypair.tf
+++ Class-1/Example-1/keypair.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_key_pair" "bastion-key" {
   key_name   = "bastion-key"
   public_key = file("~/.ssh/id_rsa.pub")
---
+++ Class-1/Example-1/sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
